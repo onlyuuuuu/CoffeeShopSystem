21f29dc1c0bdab81a5dadf9a1af0619c7cae3168
Andy <duyanhnn@outlook.com>
2023-09-18 09:30:16 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Coffee Shop System 101D
+
+> For general designs please refer to [CoffeeShopSystem101D.drawio](CoffeeShopSystem101D.drawio). You can open this from **draw.io** application or head to https://app.diagrams.net/ (Choose **File**, **Open from**, **Device**)
---
+++ core-service/src/main/groovy/com/d101/coreservice/CoreServiceApplication.groovy
@@ -0,0 +1,13 @@
+package com.d101.coreservice
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CoreServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(CoreServiceApplication, args)
+    }
+
+}
